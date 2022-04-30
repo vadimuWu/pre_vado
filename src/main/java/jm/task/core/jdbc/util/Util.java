0a23edc4087cfcb734078d011be2f07aa5217e44
vadimuWu <vadimka.zaletaev@mail.ru>
2022-05-01 00:29:23 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -7,11 +8,10 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
 public class Util {
-
-    final static String url = "jdbc:postgresql://localhost:5432/postgres";
-    final static String login = "postgres";
-    final static String password = "admin";
-    private static SessionFactory sessionFactory = null;
+//    final static String url = "jdbc:postgresql://localhost:5432/postgres";
+//    final static String login = "postgres";
+//    final static String password = "admin";
+//    private static SessionFactory sessionFactory = null;
 
     //    public static Connection getConnection() {
 //        try {
@@ -21,9 +21,24 @@ public class Util {
 //        }
 //        return con;
 //    }
+    private static final String driver = "org.postgresql.Driver";
+    private static final String url = "jdbc:postgresql://localhost:5432/postgres?useSSL=false&amp;serverTimezone=UTC";
+    private static final String login = "postgres";
+    private static final String password = "admin";
+    private static SessionFactory sessionFactory = null;
+
     public static SessionFactory getConnection() {
         try {
-            Configuration configuration = new Configuration();
+            Configuration configuration = new Configuration()
+                    .setProperty("hibernate.connection.driver_class", driver)
+                    .setProperty("hibernate.connection.url", url)
+                    .setProperty("hibernate.connection.username", login)
+                    .setProperty("hibernate.connection.password", password)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .addAnnotatedClass(User.class)
+                    .setProperty("hibernate.c3p0.min_size", "5")
+                    .setProperty("hibernate.c3p0.max_size", "200")
+                    .setProperty("hibernate.c3p0.max_statements", "200");
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
                     .applySettings(configuration.getProperties()).build();
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
